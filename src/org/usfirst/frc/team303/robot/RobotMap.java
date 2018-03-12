4ba3158cf089c61621530d3e21e119f7a2380d8c
Ashima <ashimataneja04@gmail.com>
2018-03-12 17:27:30 -0400

    Forgot RobotMap

--- src/org/usfirst/frc/team303/robot/RobotMap.java
+++ src/org/usfirst/frc/team303/robot/RobotMap.java
@@ -19,15 +19,15 @@ public class RobotMap {
 	
 	public static final int CLIMBER_WINCH_ID = 4;
 	public static final boolean CLIMBER_WINCH_INV = false;
-	public static final int CLIMBER_HOOK_DEPLOY_SOLENOID_ID = 2;
+	public static final int CLIMBER_HOOK_DEPLOY_SOLENOID_ID = 1;
 	
 	public static final int LIFT_ID = 2;
 	public static final boolean LIFT_INV = true;
 	
 	public static final int INTAKE_LEFT_ID = 15;
 	public static final int INTAKE_RIGHT_ID = 12;
-	public static final int INTAKE_ROTATION_SOLENOID_ID = 1;
-	public static final int INTAKE_ACTUATE_SOLENOID_ID = 0;
+	public static final int INTAKE_ROTATION_SOLENOID_ID = 0;
+	public static final int INTAKE_ACTUATE_SOLENOID_ID = 2;
 	public static final boolean INTAKE_LEFT_INV = false;
 	public static final boolean INTAKE_RIGHT_INV = false;
 
